ab6eeacecb871c80d732595f40c5feb83c08c1a7
MounikaAligeti <mounikaaligeti04@gmail.com>
2022-11-08 06:24:19 -0500

    code submitted

---
+++ file handlings.py
@@ -0,0 +1,14 @@
+##book=open(r"C:\Users\User\Desktop\MYfile.txt","r")
+##print(book.read())
+##
+##
+##book=open(r"C:\Users\User\Desktop\MYfile.txt","r")
+##print(book.read(5))
+##
+##
+##book=open(r"C:\Users\User\Desktop\MYfile.txt","r")
+##print(book.readline())
+
+
+book=open(r"C:\Users\User\Desktop\MYfile.txt","r")
+print(book.readlines(5))
